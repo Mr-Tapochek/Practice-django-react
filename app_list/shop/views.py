cdb06766ae20e8ee99166047029e97dfeb17f1d1
Алексей <aleshamaksimof06@gmail.com>
2025-12-18 16:44:13 +0400

    Модели и включение rest
    создали две модели (будут редактироваться), а также включили rest framework ура

--- app_list/shop/views.py
+++ app_list/shop/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Product
+from rest_framework import generics
+from .serializers import ProductSerializers
 
-# Create your views here.
+class ProductListCreate(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializers
